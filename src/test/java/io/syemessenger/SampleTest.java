79ad7ecf58e37e3bf05320b55434c73d9c22466d
Artem Vysochyn <artem.vysochyn@gmail.com>
2025-01-18 21:21:17 +0200

    Cleanup from spritg-boot

--- src/test/java/io/syemessenger/SampleTest.java
+++ src/test/java/io/syemessenger/SampleTest.java
@@ -3,25 +3,14 @@ package io.syemessenger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
 
 public class SampleTest {
 
-  private static ConfigurableApplicationContext applicationContext;
-
   @BeforeAll
-  static void beforeAll() {
-    applicationContext = SpringApplication.run(WebSocketApplication.class);
-  }
+  static void beforeAll() {}
 
   @AfterAll
-  static void afterAll() {
-    if (applicationContext != null) {
-      applicationContext.close();
-    }
-  }
+  static void afterAll() {}
 
   @Test
   void testHello() {
