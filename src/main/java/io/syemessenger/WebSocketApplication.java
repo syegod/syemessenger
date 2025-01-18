887c6447f6966a7c9fbc4df52002c58742660a8b
syegod <ovisochin362@gmail.com>
2025-01-18 19:32:11 +0100

    sample test

---
+++ src/main/java/io/syemessenger/WebSocketApplication.java
@@ -0,0 +1,6 @@
+package io.syemessenger;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketApplication {}
--- src/test/java/io/syemessenger/SampleTest.java
+++ src/test/java/io/syemessenger/SampleTest.java
@@ -1,3 +1,30 @@
 package io.syemessenger;
 
-public class SampleTest {}
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+public class SampleTest {
+
+  private static ConfigurableApplicationContext applicationContext;
+
+  @BeforeAll
+  static void beforeAll() {
+    applicationContext = SpringApplication.run(WebSocketApplication.class);
+  }
+
+  @AfterAll
+  static void afterAll() {
+    if (applicationContext != null) {
+      applicationContext.close();
+    }
+  }
+
+  @Test
+  void testHello() {
+    System.out.println("test");
+  }
+}
