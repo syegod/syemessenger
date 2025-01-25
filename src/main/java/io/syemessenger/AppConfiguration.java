684f1b161d7a9583df84c2e01d93125afacce243
syegod <ovisochin362@gmail.com>
2025-01-25 12:25:16 +0100

    wip (hikari)

--- src/main/java/io/syemessenger/AppConfiguration.java
+++ src/main/java/io/syemessenger/AppConfiguration.java
@@ -1,24 +1,32 @@
 package io.syemessenger;
 
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.syemessenger.websocket.WebSocketServer;
 import io.syemessenger.websocket.WebSocketServlet;
+import jakarta.persistence.EntityManagerFactory;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import liquibase.Contexts;
+import javax.sql.DataSource;
 import liquibase.Liquibase;
-import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
-import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan(basePackages = "io.syemessenger")
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class AppConfiguration {
 
   @Bean
@@ -32,9 +40,8 @@ public class AppConfiguration {
   }
 
   @Bean
-  public Liquibase liquibase(ServiceConfig config) throws Exception {
-    try (Connection connection =
-        DriverManager.getConnection(config.dbUrl(), config.dbUser(), config.dbPassword())) {
+  public Liquibase liquibase(DataSource dataSource) throws Exception {
+    try (Connection connection = dataSource.getConnection()) {
       final var database =
           DatabaseFactory.getInstance()
               .findCorrectDatabaseImplementation(new JdbcConnection(connection));
@@ -45,5 +52,35 @@ public class AppConfiguration {
 
       return liquibase;
     }
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    vendorAdapter.setShowSql(true);
+
+    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+    factory.setJpaVendorAdapter(vendorAdapter);
+    factory.setPackagesToScan("io.syemessenger");
+    factory.setDataSource(dataSource);
+    return factory;
+  }
+
+  @Bean
+  public DataSource dataSource(ServiceConfig config) {
+    final var hikariConfig = new HikariConfig();
+    hikariConfig.setJdbcUrl(config.dbUrl());
+    hikariConfig.setUsername(config.dbUser());
+    hikariConfig.setPassword(config.dbPassword());
+    hikariConfig.setConnectionInitSql("CREATE SCHEMA IF NOT EXISTS syemessenger");
+    hikariConfig.setSchema("syemessenger");
+    return new HikariDataSource(hikariConfig);
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+    JpaTransactionManager txManager = new JpaTransactionManager();
+    txManager.setEntityManagerFactory(entityManagerFactory);
+    return txManager;
   }
 }
