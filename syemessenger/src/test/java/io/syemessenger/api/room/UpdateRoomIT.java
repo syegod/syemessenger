fe2b16e623bf5e16c5b64488616d6395e0944e41
Artem Vysochyn <artem.vysochyn@gmail.com>
2025-02-04 23:07:04 +0200

    Done Upda

--- syemessenger/src/test/java/io/syemessenger/api/room/UpdateRoomIT.java
+++ syemessenger/src/test/java/io/syemessenger/api/room/UpdateRoomIT.java
@@ -1,7 +1,7 @@
 package io.syemessenger.api.room;
 
 import static io.syemessenger.api.ErrorAssertions.assertError;
-import static io.syemessenger.api.account.AccountAssertions.createAccount;
+import static io.syemessenger.api.account.AccountAssertions.login;
 import static io.syemessenger.api.room.RoomAssertions.createRoom;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -9,56 +9,25 @@ import static org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUti
 
 import io.syemessenger.api.ClientSdk;
 import io.syemessenger.api.account.AccountInfo;
-import io.syemessenger.api.account.AccountSdk;
-import io.syemessenger.api.account.LoginAccountRequest;
-import io.syemessenger.environment.CloseHelper;
 import io.syemessenger.environment.IntegrationEnvironmentExtension;
+import java.util.function.Function;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 @ExtendWith(IntegrationEnvironmentExtension.class)
 public class UpdateRoomIT {
 
-  private static AccountInfo accountInfo1;
-  private static AccountInfo accountInfo2;
-  private static RoomInfo existingRoomInfo;
-
-  private ClientSdk clientSdk;
-  private AccountSdk accountSdk;
-  private RoomSdk roomSdk;
-
-  @BeforeAll
-  static void beforeAll() {
-    accountInfo1 = createAccount();
-    accountInfo2 = createAccount();
-    existingRoomInfo = createRoom(accountInfo1);
-  }
-
-  @BeforeEach
-  void beforeEach() {
-    clientSdk = new ClientSdk();
-    accountSdk = clientSdk.api(AccountSdk.class);
-    roomSdk = clientSdk.api(RoomSdk.class);
-  }
-
-  @AfterEach
-  void afterEach() {
-    CloseHelper.close(clientSdk);
-  }
-
   @Test
-  void testUpdateRoomNotLoggedIn() {
+  void testUpdateRoomNotLoggedIn(ClientSdk clientSdk, AccountInfo accountInfo) {
     try {
+      final var roomInfo = createRoom(accountInfo);
       final var description = randomAlphanumeric(20);
-      roomSdk.updateRoom(
-          new UpdateRoomRequest().roomId(existingRoomInfo.id()).description(description));
+      clientSdk
+          .roomSdk()
+          .updateRoom(new UpdateRoomRequest().roomId(roomInfo.id()).description(description));
       fail("Expected exception");
     } catch (Exception ex) {
       assertError(ex, 401, "Not authenticated");
@@ -66,14 +35,17 @@ public class UpdateRoomIT {
   }
 
   @Test
-  void testUpdateRoomNotOwner() {
+  void testUpdateRoomNotOwner(
+      ClientSdk clientSdk, AccountInfo accountInfo, AccountInfo anotherAccountInfo) {
     try {
-      accountSdk.login(
-          new LoginAccountRequest().username(accountInfo2.username()).password("test12345"));
-      roomSdk.updateRoom(
-          new UpdateRoomRequest()
-              .roomId(existingRoomInfo.id())
-              .description(randomAlphanumeric(20)));
+      final var roomInfo = createRoom(accountInfo);
+
+      login(clientSdk, anotherAccountInfo);
+
+      clientSdk
+          .roomSdk()
+          .updateRoom(
+              new UpdateRoomRequest().roomId(roomInfo.id()).description(randomAlphanumeric(20)));
       fail("Expected exception");
     } catch (Exception ex) {
       assertError(ex, 403, "Not allowed");
@@ -82,63 +54,67 @@ public class UpdateRoomIT {
 
   @ParameterizedTest(name = "{0}")
   @MethodSource(value = "testUpdateRoomFailedMethodSource")
-  void testUpdateRoomFailed(
-      String test, UpdateRoomRequest request, int errorCode, String errorMessage) {
-    accountSdk.login(
-        new LoginAccountRequest().username(accountInfo1.username()).password("test12345"));
+  void testUpdateRoomFailed(FailedArgs args, ClientSdk clientSdk, AccountInfo accountInfo) {
+    final var roomInfo = createRoom(accountInfo);
+    login(clientSdk, accountInfo);
     try {
-      roomSdk.updateRoom(request);
+      clientSdk.roomSdk().updateRoom(args.request.apply(roomInfo));
       fail("Expected exception");
     } catch (Exception ex) {
-      assertError(ex, errorCode, errorMessage);
+      assertError(ex, args.errorCode, args.errorMessage);
     }
   }
 
-  private static Stream<Arguments> testUpdateRoomFailedMethodSource() {
+  private record FailedArgs(
+      String test,
+      Function<RoomInfo, UpdateRoomRequest> request,
+      int errorCode,
+      String errorMessage) {}
+
+  private static Stream<?> testUpdateRoomFailedMethodSource() {
     return Stream.of(
-        Arguments.of(
+        new FailedArgs(
             "No room id",
-            new UpdateRoomRequest().description(randomAlphanumeric(20)),
+            roomInfo -> new UpdateRoomRequest().description(randomAlphanumeric(20)),
             400,
             "Missing or invalid: roomId"),
-        Arguments.of(
+        new FailedArgs(
             "Wrong room id",
-            new UpdateRoomRequest().roomId(100L).description(randomAlphanumeric(20)),
+            roomInfo -> new UpdateRoomRequest().roomId(100L).description(randomAlphanumeric(20)),
             404,
             "Room not found"),
-        Arguments.of(
+        new FailedArgs(
             "No description",
-            new UpdateRoomRequest().roomId(existingRoomInfo.id()),
+            roomInfo -> new UpdateRoomRequest().roomId(roomInfo.id()),
             400,
             "Missing or invalid: description"),
-        Arguments.of(
+        new FailedArgs(
             "Description too short",
-            new UpdateRoomRequest()
-                .roomId(existingRoomInfo.id())
-                .description(randomAlphanumeric(5)),
+            roomInfo ->
+                new UpdateRoomRequest().roomId(roomInfo.id()).description(randomAlphanumeric(5)),
             400,
             "Missing or invalid: description"),
-        Arguments.of(
+        new FailedArgs(
             "Description too long",
-            new UpdateRoomRequest()
-                .roomId(existingRoomInfo.id())
-                .description(randomAlphanumeric(201)),
+            roomInfo ->
+                new UpdateRoomRequest().roomId(roomInfo.id()).description(randomAlphanumeric(201)),
             400,
             "Missing or invalid: description"));
   }
 
   @Test
-  void testUpdateRoom() {
-    accountSdk.login(
-        new LoginAccountRequest().username(accountInfo1.username()).password("test12345"));
+  void testUpdateRoom(ClientSdk clientSdk, AccountInfo accountInfo) {
+    final var origRoomInfo = createRoom(accountInfo);
+    login(clientSdk, accountInfo);
 
     final var description = randomAlphanumeric(20);
     final var roomInfo =
-        roomSdk.updateRoom(
-            new UpdateRoomRequest().roomId(existingRoomInfo.id()).description(description));
+        clientSdk
+            .roomSdk()
+            .updateRoom(new UpdateRoomRequest().roomId(origRoomInfo.id()).description(description));
 
-    assertEquals(existingRoomInfo.id(), roomInfo.id(), "roomInfo.id: " + roomInfo.id());
-    assertEquals(accountInfo1.username(), roomInfo.owner());
-    assertEquals(description, roomInfo.description());
+    assertEquals(origRoomInfo.id(), roomInfo.id(), "roomInfo.id: " + roomInfo.id());
+    assertEquals(accountInfo.username(), roomInfo.owner(), "roomInfo.owner");
+    assertEquals(description, roomInfo.description(), "roomInfo.description");
   }
 }
