8637292c9736233ba9676e9ca5008c898a905cc5
Oleh Vysochyn <ovisochin362@gmail.com>
2025-02-05 12:38:00 +0100

    WIP GetAccountIT

--- syemessenger/src/test/java/io/syemessenger/api/account/GetAccountIT.java
+++ syemessenger/src/test/java/io/syemessenger/api/account/GetAccountIT.java
@@ -3,15 +3,13 @@ package io.syemessenger.api.account;
 import static io.syemessenger.api.ErrorAssertions.assertError;
 import static io.syemessenger.api.account.AccountAssertions.assertAccount;
 import static io.syemessenger.api.account.AccountAssertions.createAccount;
+import static io.syemessenger.api.account.AccountAssertions.login;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import io.syemessenger.api.ClientSdk;
 import io.syemessenger.environment.CloseHelper;
-import io.syemessenger.environment.IntegrationEnvironment;
 import io.syemessenger.environment.IntegrationEnvironmentExtension;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,28 +17,24 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @ExtendWith(IntegrationEnvironmentExtension.class)
 public class GetAccountIT {
 
-  private ClientSdk clientSdk;
-  private AccountSdk accountSdk;
-  private AccountInfo existingAccountInfo;
-  private AccountInfo anotherAccountInfo;
+//  private ClientSdk clientSdk;
+//  private AccountSdk accountSdk;
+//  private AccountInfo existingAccountInfo;
+//  private AccountInfo anotherAccountInfo;
+//
+//  @BeforeEach
+//  void beforeEach() {
+//    clientSdk = new ClientSdk();
+//    accountSdk = clientSdk.api(AccountSdk.class);
+//    existingAccountInfo = createAccount();
+//    anotherAccountInfo = createAccount();
+//  }
 
-  @BeforeEach
-  void beforeEach() {
-    clientSdk = new ClientSdk();
-    accountSdk = clientSdk.api(AccountSdk.class);
-    existingAccountInfo = createAccount();
-    anotherAccountInfo = createAccount();
-  }
-
-  @AfterEach
-  void afterEach() {
-    CloseHelper.close(clientSdk);
-  }
 
   @Test
-  void testGetAccountNotLoggedIn() {
+  void testGetAccountNotLoggedIn(ClientSdk clientSdk, AccountInfo accountInfo) {
     try {
-      accountSdk.getAccount(existingAccountInfo.id());
+      clientSdk.accountSdk().getAccount(accountInfo.id());
       fail("Expected exception");
     } catch (Exception ex) {
       assertError(ex, 401, "Not authenticated");
@@ -48,20 +42,15 @@ public class GetAccountIT {
   }
 
   @Test
-  void testGetForeignAccount() {
-    accountSdk.login(
-        new LoginAccountRequest().username(existingAccountInfo.username()).password("test12345"));
+  void testGetForeignAccount(ClientSdk clientSdk, AccountInfo accountInfo, AccountInfo anotherAccountInfo) {
+    login(clientSdk, accountInfo);
     final var id = anotherAccountInfo.id();
-    assertAccount(anotherAccountInfo, accountSdk.getAccount(id));
+    assertAccount(anotherAccountInfo, clientSdk.accountSdk().getAccount(id));
   }
 
   @Test
-  void testGetOwnAccount() {
-    final var accountId =
-        accountSdk.login(
-            new LoginAccountRequest()
-                .username(existingAccountInfo.username())
-                .password("test12345"));
-    assertAccount(existingAccountInfo, accountSdk.getAccount(accountId));
+  void testGetOwnAccount(ClientSdk clientSdk, AccountInfo accountInfo) {
+    login(clientSdk, accountInfo);
+    assertAccount(accountInfo, clientSdk.accountSdk().getAccount(accountInfo.id()));
   }
 }
