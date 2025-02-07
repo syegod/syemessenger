d801fd4ed212794b396bc97a2d5628d4cee54ded
Oleh <97557244+syegod@users.noreply.github.com>
2025-02-07 15:45:38 +0100

    Revert "Issue20 remove bcrypt (#22)" (#23)
    This reverts commit c5ccfccc9538574f42251e70343cf042d08b0a2b.

---
+++ syemessenger/src/main/java/io/syemessenger/api/account/PasswordHashing.java
@@ -0,0 +1,14 @@
+package io.syemessenger.api.account;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+public class PasswordHashing {
+
+  public static String hash(String password) {
+    return BCrypt.hashpw(password, BCrypt.gensalt(10));
+  }
+
+  public static boolean check(String password, String hash) {
+    return BCrypt.checkpw(password, hash);
+  }
+}
