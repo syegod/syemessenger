9db1485e117f5fbcde045ca5dc13e10fc75b90b5
Oleh Vysochyn <ovisochin362@gmail.com>
2025-02-10 15:22:06 +0100

    DONE roomService DONE getRoomMembersIT failed when non-member uses it

--- syemessenger/src/main/java/io/syemessenger/api/room/RoomController.java
+++ syemessenger/src/main/java/io/syemessenger/api/room/RoomController.java
@@ -1,42 +1,23 @@
 package io.syemessenger.api.room;
 
-import static io.syemessenger.api.Pageables.toPageable;
 
 import io.syemessenger.annotations.RequestController;
 import io.syemessenger.annotations.RequestHandler;
 import io.syemessenger.api.ServiceException;
 import io.syemessenger.api.ServiceMessage;
 import io.syemessenger.api.account.AccountMappers;
-import io.syemessenger.api.account.repository.AccountRepository;
-import io.syemessenger.api.room.repository.BlockedMember;
-import io.syemessenger.api.room.repository.BlockedMemberId;
-import io.syemessenger.api.room.repository.BlockedRepository;
-import io.syemessenger.api.room.repository.Room;
-import io.syemessenger.api.room.repository.RoomRepository;
 import io.syemessenger.websocket.SessionContext;
 import jakarta.inject.Named;
-import java.time.Clock;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.domain.Page;
 
 @Named
 @RequestController
 public class RoomController {
 
-  private final RoomRepository roomRepository;
-  private final AccountRepository accountRepository;
-  private final BlockedRepository blockedRepository;
+  private final RoomService roomService;
 
-  public RoomController(
-      RoomRepository roomRepository,
-      AccountRepository accountRepository,
-      BlockedRepository blockedRepository) {
-    this.roomRepository = roomRepository;
-    this.accountRepository = accountRepository;
-    this.blockedRepository = blockedRepository;
+  public RoomController(RoomService roomService) {
+    this.roomService = roomService;
   }
 
   @RequestHandler("v1/syemessenger/createRoom")
@@ -60,20 +41,10 @@ public class RoomController {
       }
     }
 
-    final var account = accountRepository.findById(sessionContext.accountId()).orElse(null);
-    if (account == null) {
-      throw new ServiceException(404, "Account not found");
-    }
-
-    final var now = LocalDateTime.now(Clock.systemUTC()).truncatedTo(ChronoUnit.MILLIS);
-    final var room =
-        new Room().name(name).description(description).owner(account).createdAt(now).updatedAt(now);
-
     try {
-      final var saved = roomRepository.save(room);
-      roomRepository.saveRoomMember(saved.id(), account.id());
-      final var roomInfo = RoomMappers.toRoomInfo(saved);
-      sessionContext.send(new ServiceMessage().qualifier("createRoom").data(roomInfo));
+      final var room = roomService.createRoom(request, sessionContext.accountId());
+      sessionContext.send(
+          new ServiceMessage().qualifier("createRoom").data(RoomMappers.toRoomInfo(room)));
     } catch (DataAccessException e) {
       if (e.getMessage().contains("duplicate key value violates unique constraint")) {
         throw new ServiceException(400, "Cannot create room: already exists");
@@ -100,21 +71,11 @@ public class RoomController {
     if (roomId == null) {
       throw new ServiceException(400, "Missing or invalid: roomId");
     }
-    final var room = roomRepository.findById(roomId).orElse(null);
-    if (room == null) {
-      throw new ServiceException(404, "Room not found");
-    }
 
-    if (!room.owner().id().equals(sessionContext.accountId())) {
-      throw new ServiceException(403, "Not room owner");
-    }
+    final var room = roomService.updateRoom(request, sessionContext.accountId());
 
-    room.description(description);
-
-    final var saved = roomRepository.save(room);
-    final var roomInfo = RoomMappers.toRoomInfo(saved);
-
-    sessionContext.send(new ServiceMessage().qualifier("updateRoom").data(roomInfo));
+    sessionContext.send(
+        new ServiceMessage().qualifier("updateRoom").data(RoomMappers.toRoomInfo(room)));
   }
 
   @RequestHandler("v1/syemessenger/getRoom")
@@ -127,10 +88,7 @@ public class RoomController {
       throw new ServiceException(400, "Missing or invalid: id");
     }
 
-    final var room = roomRepository.findById(id).orElse(null);
-    if (room == null) {
-      throw new ServiceException(404, "Room not found");
-    }
+    final var room = roomService.getRoom(id);
 
     sessionContext.send(
         new ServiceMessage().qualifier("getRoom").data(RoomMappers.toRoomInfo(room)));
@@ -149,29 +107,15 @@ public class RoomController {
       throw new ServiceException(400, "Missing or invalid: name");
     }
 
-    final var room = roomRepository.findByName(name);
-    if (room == null) {
-      throw new ServiceException(404, "Room not found");
-    }
-
-    final var blockedMember =
-        blockedRepository
-            .findById(new BlockedMemberId().roomId(room.id()).accountId(sessionContext.accountId()))
-            .orElse(null);
-    if (blockedMember != null) {
-      throw new ServiceException(400, "Cannot join room: blocked");
-    }
-
     try {
-      roomRepository.saveRoomMember(room.id(), sessionContext.accountId());
+      final var roomId = roomService.joinRoom(name, sessionContext.accountId());
+      sessionContext.send(new ServiceMessage().qualifier("joinRoom").data(roomId));
     } catch (DataAccessException e) {
       if (e.getMessage().contains("duplicate key value violates unique constraint")) {
         throw new ServiceException(400, "Cannot join room: already joined");
       }
       throw e;
     }
-
-    sessionContext.send(new ServiceMessage().qualifier("joinRoom").data(room.id()));
   }
 
   @RequestHandler("v1/syemessenger/leaveRoom")
@@ -184,23 +128,9 @@ public class RoomController {
       throw new ServiceException(400, "Missing or invalid: id");
     }
 
-    final var room = roomRepository.findById(id).orElse(null);
-    if (room == null) {
-      throw new ServiceException(404, "Room not found");
-    }
+    final var roomId = roomService.leaveRoom(id, sessionContext.accountId());
 
-    final var roomMember = roomRepository.findRoomMember(id, sessionContext.accountId());
-    if (roomMember == null) {
-      throw new ServiceException(400, "Cannot leave room: not joined");
-    }
-
-    if (room.owner().id().equals(sessionContext.accountId())) {
-      roomRepository.deleteById(id);
-      sessionContext.send(new ServiceMessage().qualifier("leaveRoom").data(id));
-    } else {
-      roomRepository.deleteRoomMember(id, sessionContext.accountId());
-      sessionContext.send(new ServiceMessage().qualifier("leaveRoom").data(id));
-    }
+    sessionContext.send(new ServiceMessage().qualifier("leaveRoom").data(roomId));
   }
 
   @RequestHandler("v1/syemessenger/removeRoomMembers")
@@ -222,20 +152,7 @@ public class RoomController {
       throw new ServiceException(400, "Missing or invalid: memberIds");
     }
 
-    final var room = roomRepository.findById(roomId).orElse(null);
-    if (room == null) {
-      throw new ServiceException(404, "Room not found");
-    }
-
-    if (!sessionContext.accountId().equals(room.owner().id())) {
-      throw new ServiceException(403, "Not room owner");
-    }
-
-    if (memberIds.contains(room.owner().id())) {
-      throw new ServiceException(400, "Cannot remove room owner");
-    }
-
-    roomRepository.deleteRoomMembers(roomId, memberIds);
+    roomService.removeRoomMembers(request, sessionContext.accountId());
 
     sessionContext.send(new ServiceMessage().qualifier("removeRoomMembers").data(roomId));
   }
@@ -259,27 +176,8 @@ public class RoomController {
       throw new ServiceException(400, "Missing or invalid: memberIds");
     }
 
-    final var room = roomRepository.findById(roomId).orElse(null);
-    if (room == null) {
-      throw new ServiceException(404, "Room not found");
-    }
-
-    if (!sessionContext.accountId().equals(room.owner().id())) {
-      throw new ServiceException(403, "Not room owner");
-    }
-
-    if (memberIds.contains(room.owner().id())) {
-      throw new ServiceException(400, "Cannot block room owner");
-    }
-
-    final var blockedMembers = new ArrayList<BlockedMember>();
-    for (var memberId : memberIds) {
-      blockedMembers.add(new BlockedMember().roomId(roomId).accountId(memberId));
-    }
-
     try {
-      roomRepository.deleteRoomMembers(roomId, memberIds);
-      blockedRepository.saveAll(blockedMembers);
+      roomService.blockRoomMembers(request, sessionContext.accountId());
       sessionContext.send(new ServiceMessage().qualifier("blockRoomMembers").data(roomId));
     } catch (Exception e) {
       if (e.getMessage().contains("is not present in table")) {
@@ -308,21 +206,7 @@ public class RoomController {
       throw new ServiceException(400, "Missing or invalid: memberIds");
     }
 
-    final var room = roomRepository.findById(roomId).orElse(null);
-    if (room == null) {
-      throw new ServiceException(404, "Room not found");
-    }
-
-    if (!sessionContext.accountId().equals(room.owner().id())) {
-      throw new ServiceException(403, "Not room owner");
-    }
-
-    final var blockedMembers = new ArrayList<BlockedMember>();
-    for (var memberId : memberIds) {
-      blockedMembers.add(new BlockedMember().roomId(roomId).accountId(memberId));
-    }
-
-    blockedRepository.deleteAll(blockedMembers);
+    roomService.unblockRoomMembers(request, sessionContext.accountId());
 
     sessionContext.send(new ServiceMessage().qualifier("unblockRoomMembers").data(roomId));
   }
@@ -348,18 +232,7 @@ public class RoomController {
       throw new ServiceException(400, "Missing or invalid: roomId");
     }
 
-    final var room = roomRepository.findById(roomId).orElse(null);
-    if (room == null) {
-      throw new ServiceException(404, "Room not found");
-    }
-
-    if (!room.owner().id().equals(sessionContext.accountId())) {
-      throw new ServiceException(403, "Not room owner");
-    }
-
-    final var pageable = toPageable(offset, limit, request.orderBy());
-
-    final var accountPage = roomRepository.findBlockedMembers(roomId, pageable);
+    final var accountPage = roomService.getBlockedMembers(request, sessionContext.accountId());
 
     final var accountInfos =
         accountPage.getContent().stream().map(AccountMappers::toAccountInfo).toList();
@@ -390,25 +263,16 @@ public class RoomController {
       throw new ServiceException(400, "Missing or invalid: limit");
     }
 
-    final var pageable = toPageable(offset, limit, request.orderBy());
+    final var roomsPage = roomService.listRooms(request);
 
-    var keyword = request.keyword();
-    Page<Room> roomsPages;
-    if (keyword == null) {
-      roomsPages = roomRepository.findAll(pageable);
-    } else {
-      roomsPages =
-          roomRepository.findByNameContainingOrDescriptionContaining(keyword, keyword, pageable);
-    }
-
-    final var roomInfos = roomsPages.getContent().stream().map(RoomMappers::toRoomInfo).toList();
+    final var roomInfos = roomsPage.getContent().stream().map(RoomMappers::toRoomInfo).toList();
 
     final var listRoomsResponse =
         new ListRoomsResponse()
             .roomInfos(roomInfos)
             .offset(offset)
             .limit(limit)
-            .totalCount(roomsPages.getTotalElements());
+            .totalCount(roomsPage.getTotalElements());
 
     sessionContext.send(new ServiceMessage().qualifier("listRooms").data(listRoomsResponse));
   }
@@ -434,19 +298,17 @@ public class RoomController {
       throw new ServiceException(400, "Missing or invalid: limit");
     }
 
-    final var pages =
-        roomRepository.findRoomMembers(
-            request.roomId(), toPageable(offset, limit, request.orderBy()));
+    final var membersPage = roomService.getRoomMembers(request, sessionContext.accountId());
 
     final var accountInfos =
-        pages.getContent().stream().map(AccountMappers::toAccountInfo).toList();
+        membersPage.getContent().stream().map(AccountMappers::toAccountInfo).toList();
 
     GetRoomMembersResponse response =
         new GetRoomMembersResponse()
             .accountInfos(accountInfos)
             .offset(offset)
             .limit(limit)
-            .totalCount(pages.getTotalElements());
+            .totalCount(membersPage.getTotalElements());
 
     sessionContext.send(new ServiceMessage().qualifier("getRoomMembers").data(response));
   }
---
+++ syemessenger/src/main/java/io/syemessenger/api/room/RoomService.java
@@ -0,0 +1,229 @@
+package io.syemessenger.api.room;
+
+import static io.syemessenger.api.Pageables.toPageable;
+
+import io.syemessenger.api.ServiceException;
+import io.syemessenger.api.account.repository.Account;
+import io.syemessenger.api.account.repository.AccountRepository;
+import io.syemessenger.api.room.repository.BlockedMember;
+import io.syemessenger.api.room.repository.BlockedMemberId;
+import io.syemessenger.api.room.repository.BlockedRepository;
+import io.syemessenger.api.room.repository.Room;
+import io.syemessenger.api.room.repository.RoomRepository;
+import jakarta.inject.Named;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import org.springframework.data.domain.Page;
+import org.springframework.transaction.annotation.Transactional;
+
+@Named
+@Transactional
+public class RoomService {
+
+  private final RoomRepository roomRepository;
+  private final AccountRepository accountRepository;
+  private final BlockedRepository blockedRepository;
+
+  public RoomService(
+      RoomRepository roomRepository,
+      AccountRepository accountRepository,
+      BlockedRepository blockedRepository) {
+    this.roomRepository = roomRepository;
+    this.accountRepository = accountRepository;
+    this.blockedRepository = blockedRepository;
+  }
+
+  public Room createRoom(CreateRoomRequest request, Long accountId) {
+
+    final var account = accountRepository.findById(accountId).orElse(null);
+    if (account == null) {
+      throw new ServiceException(404, "Account not found");
+    }
+
+    final var now = LocalDateTime.now(Clock.systemUTC()).truncatedTo(ChronoUnit.MILLIS);
+    final var room =
+        new Room()
+            .name(request.name())
+            .description(request.description())
+            .owner(account)
+            .createdAt(now)
+            .updatedAt(now);
+
+    final var saved = roomRepository.save(room);
+    roomRepository.saveRoomMember(saved.id(), account.id());
+    return saved;
+  }
+
+  public Room updateRoom(UpdateRoomRequest request, Long accountId) {
+
+    final var room = roomRepository.findById(request.roomId()).orElse(null);
+    if (room == null) {
+      throw new ServiceException(404, "Room not found");
+    }
+
+    if (!room.owner().id().equals(accountId)) {
+      throw new ServiceException(403, "Not room owner");
+    }
+
+    room.description(request.description());
+
+    return roomRepository.save(room);
+  }
+
+  @Transactional(readOnly = true)
+  public Room getRoom(Long id) {
+    final var room = roomRepository.findById(id).orElse(null);
+    if (room == null) {
+      throw new ServiceException(404, "Room not found");
+    }
+
+    return room;
+  }
+
+  public Long joinRoom(String name, Long accountId) {
+
+    final var room = roomRepository.findByName(name);
+    if (room == null) {
+      throw new ServiceException(404, "Room not found");
+    }
+
+    final var blockedMember =
+        blockedRepository
+            .findById(new BlockedMemberId().roomId(room.id()).accountId(accountId))
+            .orElse(null);
+    if (blockedMember != null) {
+      throw new ServiceException(400, "Cannot join room: blocked");
+    }
+
+    roomRepository.saveRoomMember(room.id(), accountId);
+
+    return room.id();
+  }
+
+  public Long leaveRoom(Long roomId, Long accountId) {
+    final var room = roomRepository.findById(roomId).orElse(null);
+    if (room == null) {
+      throw new ServiceException(404, "Room not found");
+    }
+
+    final var roomMember = roomRepository.findRoomMember(roomId, accountId);
+    if (roomMember == null) {
+      throw new ServiceException(400, "Cannot leave room: not joined");
+    }
+
+    if (room.owner().id().equals(accountId)) {
+      roomRepository.deleteById(roomId);
+    } else {
+      roomRepository.deleteRoomMember(roomId, accountId);
+    }
+    return roomId;
+  }
+
+  public Long removeRoomMembers(RemoveMembersRequest request, Long accountId) {
+
+    final var room = roomRepository.findById(request.roomId()).orElse(null);
+    if (room == null) {
+      throw new ServiceException(404, "Room not found");
+    }
+
+    if (!room.owner().id().equals(accountId)) {
+      throw new ServiceException(403, "Not room owner");
+    }
+
+    if (request.memberIds().contains(room.owner().id())) {
+      throw new ServiceException(400, "Cannot remove room owner");
+    }
+
+    roomRepository.deleteRoomMembers(request.roomId(), request.memberIds());
+    return request.roomId();
+  }
+
+  public Long blockRoomMembers(BlockMembersRequest request, Long accountId) {
+    final var room = roomRepository.findById(request.roomId()).orElse(null);
+    if (room == null) {
+      throw new ServiceException(404, "Room not found");
+    }
+
+    if (!room.owner().id().equals(accountId)) {
+      throw new ServiceException(403, "Not room owner");
+    }
+
+    if (request.memberIds().contains(room.owner().id())) {
+      throw new ServiceException(400, "Cannot block room owner");
+    }
+
+    final var blockedMembers = new ArrayList<BlockedMember>();
+    for (var memberId : request.memberIds()) {
+      blockedMembers.add(new BlockedMember().roomId(request.roomId()).accountId(memberId));
+    }
+
+    roomRepository.deleteRoomMembers(request.roomId(), request.memberIds());
+    blockedRepository.saveAll(blockedMembers);
+    return room.id();
+  }
+
+  public Long unblockRoomMembers(UnblockMembersRequest request, Long accountId) {
+    final var room = roomRepository.findById(request.roomId()).orElse(null);
+    if (room == null) {
+      throw new ServiceException(404, "Room not found");
+    }
+
+    if (!room.owner().id().equals(accountId)) {
+      throw new ServiceException(403, "Not room owner");
+    }
+
+    final var blockedMembers = new ArrayList<BlockedMember>();
+    for (var memberId : request.memberIds()) {
+      blockedMembers.add(new BlockedMember().roomId(request.roomId()).accountId(memberId));
+    }
+
+    blockedRepository.deleteAll(blockedMembers);
+    return request.roomId();
+  }
+
+  @Transactional(readOnly = true)
+  public Page<Account> getBlockedMembers(GetBlockedMembersRequest request, Long accountId) {
+    final var room = roomRepository.findById(request.roomId()).orElse(null);
+    if (room == null) {
+      throw new ServiceException(404, "Room not found");
+    }
+
+    if (!room.owner().id().equals(accountId)) {
+      throw new ServiceException(403, "Not room owner");
+    }
+
+    final var pageable = toPageable(request.offset(), request.limit(), request.orderBy());
+
+    return roomRepository.findBlockedMembers(request.roomId(), pageable);
+  }
+
+  @Transactional(readOnly = true)
+  public Page<Room> listRooms(ListRoomsRequest request) {
+    final var pageable = toPageable(request.offset(), request.limit(), request.orderBy());
+
+    var keyword = request.keyword();
+    Page<Room> roomPage;
+    if (keyword == null) {
+      roomPage = roomRepository.findAll(pageable);
+    } else {
+      roomPage =
+          roomRepository.findByNameContainingOrDescriptionContaining(keyword, keyword, pageable);
+    }
+
+    return roomPage;
+  }
+
+  @Transactional(readOnly = true)
+  public Page<Account> getRoomMembers(GetRoomMembersRequest request, Long accountId) {
+
+    final var roomMember = roomRepository.findRoomMember(request.roomId(), accountId);
+    if (roomMember == null) {
+      throw new ServiceException(403, "Not room member");
+    }
+
+    return roomRepository.findRoomMembers(
+        request.roomId(), toPageable(request.offset(), request.limit(), request.orderBy()));
+  }
+}
